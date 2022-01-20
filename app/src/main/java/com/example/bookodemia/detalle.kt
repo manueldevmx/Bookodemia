6c44077751b1dfe07a3faa264b271d40894f71ff
manueldevmx <43160136+manueldevmx@users.noreply.github.com>
2022-01-19 19:57:49 -0600

    Detalle_libro

---
+++ app/src/main/java/com/example/bookodemia/detalle.kt
@@ -0,0 +1,11 @@
+package com.example.bookodemia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class detalle : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle)
+    }
+}
