f66492797d24a8229588302d88cf7d9992a80bfb
manueldevmx <43160136+manueldevmx@users.noreply.github.com>
2022-01-22 00:47:12 -0600

    se añadio intent al boton de inicio de sesion

--- app/src/main/java/com/example/bookodemia/LoginActivity.kt
+++ app/src/main/java/com/example/bookodemia/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.example.bookodemia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_iniciar = findViewById<Button>(R.id.btn_iniciar)
+
+        btn_iniciar.setOnClickListener {
+            val intent = Intent(this, SignIn::class.java)
+            startActivity(intent)
+        }
     }
 }
